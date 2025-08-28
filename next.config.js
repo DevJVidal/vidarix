a9389f7a684d03c12fbf41ded5ee8dd85c28ab42
Janderson Dias <jandersonvidalvidal14@gmail.com>
2025-08-28 13:18:50 -0300

    Configuração para GitHub Pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/vidarix",
+  assetPrefix: "/vidarix/",
+};
+
+module.exports = nextConfig;
